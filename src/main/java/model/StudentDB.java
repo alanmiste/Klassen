3aea7b355bc8aa7ad1c90c7567a68da1c100102d
Alan <baranalaan@gmail.com>
2022-07-06 15:34:46 +0200

    add HashMap

--- src/main/java/model/StudentDB.java
+++ src/main/java/model/StudentDB.java
@@ -1,22 +1,23 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class StudentDB {
 
-    private ArrayList<Student> studentArray;
+    private HashMap<Integer, ArrayList<Student>> studentArray;
 
     public StudentDB(){
         Student student1 = new Student(1,"Albert","Einstein",143);
         Student student2 = new Student(2,"Albert2","Einstein2",144);
         Student student3 = new Student(3,"Albert3","Einstein3",145);
-        this.studentArray = new ArrayList<>();
-        this.studentArray.add(student1);
-        this.studentArray.add(student2);
-        this.studentArray.add(student3);
+        this.studentArray = new HashMap<>();
+        this.studentArray.put(student1.getId(),student1);
+//        this.studentArray.put(student2.getId(),student2);
+//        this.studentArray.put(student3.getId(),student3);
     }
 
-    public ArrayList<Student> getAllStudents(){
+    public HashMap<Integer, Student> getAllStudents(){
 //        for(int i=0; i<this.studentArray.length; i++)
 //            for (int j=0; j<this.studentArray[i].length; j++){
 //                System.out.println(this.studentArray[i][j]);
